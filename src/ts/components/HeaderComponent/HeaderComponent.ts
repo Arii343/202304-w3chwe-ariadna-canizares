7db698b4f579a4d9df463b3e9c148159627d24b4
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-04-23 16:32:03 +0200

    Add HeaderComponent

---
+++ src/ts/components/HeaderComponent/HeaderComponent.ts
@@ -0,0 +1,23 @@
+import Component from "../Component/Component.js";
+
+class HeaderComponent extends Component {
+  constructor(parentElement: Element) {
+    super(parentElement, "header", "header");
+
+    this.renderHtml();
+  }
+
+  renderHtml(): void {
+    this.element.innerHTML = `
+      <img src="./img/pokemon-logo.svg" alt="logo Pokémon"></img>
+      <nav class="navbar">
+        <ul class="navbar__list">
+          <li class="navbar__item"> <a class="navbar__link">Todos los Pokémon</a> </li>
+          <li class="navbar__item"> <a class="navbar__link">Mis Pokémon</a> </li>
+          <li class="navbar__item"> <a class="navbar__link">Detalle de Pokémon</a> </li>
+        </ul> 
+      </nav>`;
+  }
+}
+
+export default HeaderComponent;
