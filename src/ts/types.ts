3271070d29c137b13a9107789e0ed01be06b521c
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-04-24 00:42:20 +0200

    Add Pokemon interface to types.ts

--- src/ts/types.ts
+++ src/ts/types.ts
@@ -2,10 +2,22 @@ export interface PokemonApiResponse {
   count: number;
   next: string | undefined;
   previous: string | undefined;
-  results: PokemonApiResponse[];
+  results: PokemonApiResults[];
 }
 
 export interface PokemonApiResults {
   name: string;
   url: string;
+}
+
+export interface Pokemon {
+  name: string;
+  id: number;
+  sprites: {
+    other: {
+      "official-artwork": {
+        front_default: string;
+      };
+    };
+  };
 }
